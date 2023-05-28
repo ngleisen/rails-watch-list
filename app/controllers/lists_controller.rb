358082c7aabb4818687f55dec9bcd20f5bfc4a56
Mickael Souchet <souchet.mickael@gmail.com>
2023-05-28 13:02:49 +0200

    fonctionnalité delete list ajoutée

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,5 @@
 class ListsController < ApplicationController
-  before_action :select_list, only: [:show]
+  before_action :select_list, only: [:show, :destroy]
 
   def index
     @lists = List.all
@@ -21,6 +21,11 @@ class ListsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'lists#index'
-  resources :lists, except: [:update, :edit, :destroy] do
+  resources :lists, except: [:update, :edit] do
     resources :bookmarks, only: [:new, :create]
     resources :reviews, only: :create
   end
